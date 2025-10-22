25b95d27797dba75e047d6ea0d1f3eb3a046820a
Emann <emanneitahhh@icloud.com>
2025-10-22 20:26:46 +0300

    Add files via upload

---
+++ emaan3itahhh.py
@@ -0,0 +1,16 @@
+X=10
+Y=20
+Z=30
+budget=50
+Total_cost=X+Y+Z
+
+if Total_cost>=budget:
+    print("True")
+else:
+    print("False")
+
+differece =Total_cost-budget
+if differece>=Total_cost-budget:
+    print("money is left")
+else:
+    print("money is needed")
